07463271424aaa0de96e8d8bcffca519249cdba0
Gershell Lopez <grlopez90@gmail.com>
2021-05-03 18:20:07 -0600

    ejemplo de operaciones en clase

---
+++ ejemplos/PatronRepositorio/PatronRepositorio/Operaciones.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace PatronRepositorio
+{
+    class Operaciones
+    {
+        public void Insertar(string nombres, string apellidos, char genero, string fechaNacimiento)
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=MiBaseDeDatos; Integrated Security=true;");
+            SqlCommand comando = new SqlCommand($"INSERT INTO personas (personaId, nombres, apellidos, genero, fechaNacimiento) VALUES (NEWID(), '{nombres}', '{apellidos}', '{genero}', '{fechaNacimiento}')");
+            comando.Connection = conexion;
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void Editar(Guid personaId, string nombres, string apellidos, char genero, string fechaNacimiento)
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=MiBaseDeDatos; Integrated Security=true;");
+            SqlCommand comando = new SqlCommand($"UPDATE personas SET nombres='{nombres}', apellidos = '{apellidos}', genero = '{genero}', fechaNacimiento = '{fechaNacimiento}' WHERE personaId = '{personaId.ToString()}'");
+            comando.Connection = conexion;
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void Eliminar(Guid personaId)
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=MiBaseDeDatos; Integrated Security=true;");
+            SqlCommand comando = new SqlCommand($"DELETE FROM personas WHERE personaId = {personaId.ToString()}");
+            comando.Connection = conexion;
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+    }
+}
--- ejemplos/PatronRepositorio/SCRIPT DB.sql
+++ ejemplos/PatronRepositorio/SCRIPT DB.sql
@@ -18,3 +18,12 @@ CREATE TABLE personas (
 
 SELECT * FROM personas
 GO
+
+INSERT INTO personas (personaId, nombres, apellidos, genero, fechaNacimiento)
+VALUES (NEWID(), 'Carlitos', 'Vargas', 'M', '31/12/1990')
+GO
+
+UPDATE personas SET genero = 'F', fechaNacimiento = '27/05/1998' WHERE personaId = '631BD9CF-312F-4DBF-83A7-384CDC51C9BD'
+GO
+
+DELETE FROM personas WHERE personaId = 95C2138A-ABC9-4515-ADEA-E7C22CF2F0EC
